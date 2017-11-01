52f3bfdfdcbb34ac4077c71c82e4d2108a73fd8e
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2017-11-01 22:38:38 +0100

    Playing...

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+git config --global user.name 'Joseph Morant Navarro'
+git config --global user.email 'joseph.morantnavarro@gmail.com'
+git config --global color.ui true
+
 if [ $# -eq 1 ]
 then
 	CODEBUILD_SRC_DIR="~"
 fi
 
-mkdir -p $CODEBUILD_SRC_DIR/bin
-curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > $CODEBUILD_SRC_DIR/bin/repo && chmod a+x $CODEBUILD_SRC_DIR/bin/repo
-export PATH=$PATH:$CODEBUILD_SRC_DIR/bin
+mkdir -p $CODEBUILD_SRC_DIR/bin &&
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > $CODEBUILD_SRC_DIR/bin/repo && chmod a+x $CODEBUILD_SRC_DIR/bin/repo &&
+export PATH=$PATH:$CODEBUILD_SRC_DIR/bin &&
 
-git config --global user.name 'Joseph Morant Navarro'
-git config --global user.email 'joseph.morantnavarro@gmail.com'
-git config --global color.ui true
+mkdir -p $CODEBUILD_SRC_DIR/xenon &&
+cp -r --parents .repo $CODEBUILD_SRC_DIR/xenon &&
 
-mkdir -p $CODEBUILD_SRC_DIR/xenon
-cp -r --parents .repo $CODEBUILD_SRC_DIR/xenon
-
-cd $CODEBUILD_SRC_DIR/xenon
-repo init -u https://github.com/joseph184/platform_manifest.git -b n --depth=1
-repo sync -c -n -j 4 --quiet && repo sync -c -l -j 8 --quiet
+cd $CODEBUILD_SRC_DIR/xenon &&
+repo init -u https://github.com/joseph184/platform_manifest.git -b n --depth=1 &&
+repo sync -c -n -j 4 --quiet && repo sync -c -l -j 8 --quiet &&
 . build/envsetup.sh && breakfast hydrogen && brunch hydrogen
